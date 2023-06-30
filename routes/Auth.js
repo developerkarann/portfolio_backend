4e38ab3b8d31d7e088abb656b5cbb2399f64d803
Karan <karanpal03040@gmail.com>
2023-06-30 20:34:02 +0530

    did changes in api

--- routes/Auth.js
+++ routes/Auth.js
@@ -3,30 +3,23 @@ const router = express.Router();
 const Contact = require('../model/contectSchema')
 
 router.post('/contact', async (req, res) => {
-    // const { name, email, subject, description } = req.body;
-  
-    // if (!name || email || subject || description ) {
-    //   // res.send({data: "Not found please fill the data"})
-    // }
-    try {
-        // const userContact = new Contacts({
-        //   name, email, subject, description
-        // })
-        // await userContact.save();
-        // res.status(201).json(userContact)
-        // console.log(userContact);    
-        
-        await Contact.create({
-          name: req.body.name,
-          email: req.body.email,
-          subject: req.body.subject,
-          description: req.body.description
-        })
-        res.json({success: true})
-
-    } catch (error) {
-      res.json({ error: "Found some error" })
+  const { name, email, subject, description } = req.body
+  try {
+    if (!name || !email || !subject || !description) {
+      return res.status(404).json({ status: 'error', message: 'Please fill all the fields' })
+    } else {
+      const data = await new Contact({
+        name: name,
+        email: email,
+        subject: subject,
+        description: description
+      })
+      return res.status(200).json({ status: 'error', message: 'Data received' })
     }
-  })
+  
+  } catch (error) {
+    res.status(404).json({ error: error.message, message: 'Getting error while calling contact api' })
+  }
+})
 
-  module.exports = router;
+module.exports = router;
