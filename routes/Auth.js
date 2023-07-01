ef736cefaa24bb2d6ecb130a562b1ed534648087
Karan <karanpal03040@gmail.com>
2023-07-01 11:26:04 +0530

    Fix contact api

--- routes/Auth.js
+++ routes/Auth.js
@@ -7,16 +7,16 @@ router.post('/contact', async (req, res) => {
   try {
     if (!name || !email || !subject || !description) {
       return res.status(404).json({ status: 'error', message: 'Please fill all the fields' })
-    } else {
-      const data = await new Contact({
-        name: name,
-        email: email,
-        subject: subject,
-        description: description
-      })
-      return res.status(200).json({ status: 'error', message: 'Data received' })
     }
-  
+    const data = await new Contact({
+      name,
+      email,
+      subject,
+      description
+    })
+    await data.save()
+    return res.status(200).json({ status: 'error', message: 'Data received' })
+
   } catch (error) {
     res.status(404).json({ error: error.message, message: 'Getting error while calling contact api' })
   }
