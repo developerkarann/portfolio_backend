1b076f4a71a3373048de2fadb44021b835d94972
Karan <karanpal03040@gmail.com>
2023-07-03 21:10:12 +0530

    Fix contact api

--- routes/Contact.js
+++ routes/Contact.js
@@ -3,60 +3,69 @@ const express = require('express')
 const router = express.Router();
 const Contact = require('../model/contectSchema')
 const nodemailer = require('nodemailer');
+const { body, validationResult } = require('express-validator');
 
-router.post('/contact', async (req, res) => {
+router.post('/contact', [
+  body('email', 'Please enter a valid email').isEmail(),
+  body('name', 'Please enter a valid name').isLength({ min: 5 }),
+],
+  async (req, res) => {
 
-  const { name, email, subject, description } = req.body
-  try {
+    const { name, email, subject, description } = req.body
     if (!name || !email || !subject || !description) {
-      return res.status(404).json({ status: 'error', message: 'Please fill all the fields' })
+      return res.status(422).json({ status: 'error', message: 'Please fill all the fields' })
     }
-    const data = await new Contact({
-      name,
-      email,
-      subject,
-      description
-    })
-    await data.save()
-
-
-    // send email functionality >>>
-
-
-    // Connect with SMTP Server
-    const transporterForUser = nodemailer.createTransport({ // For User
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-      }
-    });
-
-    const transporterToMe = nodemailer.createTransport({// For Us
-      service: 'gmail',
-      auth: {
-        user: process.env.MYEMAIL,
-        pass: process.env.MYPASS
-      }
-    });
-
-
-    // Send Mail and body
-
-    // Options for getting user info/message by email
-    const MyOptions = {         // For Us
-      from: "karandeveloper03040@gmail.com",
-      to: "karanpal03040@gmail.com",
-      subject: `${name} send a message by Portfolio`,
-      html: `<h3>Name: ${name}</3><br><h3>Email: ${email}</3><br><h3>Subject:  ${subject}</3><br><h3> Message:</3> <br><br> <p>${description}</p>`
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array(), message: errors.array().map(err => err.msg).toString() });
     }
-    // Options for sending mail to the user
-    const UserOptions = {         //For USer
-      from: "karanpal03040@gmail.com",
-      to: email,
-      subject: 'Thank you for sending message!',
-      html: `
-      <h3>
+    try {
+      const data = await new Contact({
+        name,
+        email,
+        subject,
+        description
+      })
+      await data.save()
+
+
+      // send email functionality >>>
+
+
+      // Connect with SMTP Server
+      const transporterForUser = nodemailer.createTransport({ // For User
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASS
+        }
+      });
+
+      const transporterToMe = nodemailer.createTransport({// For Us
+        service: 'gmail',
+        auth: {
+          user: process.env.MYEMAIL,
+          pass: process.env.MYPASS
+        }
+      });
+
+
+      // Send Mail and body
+
+      // Options for getting user info/message by email
+      const MyOptions = {         // For Us
+        from: "karandeveloper03040@gmail.com",
+        to: "karanpal03040@gmail.com",
+        subject: `${name} send a message by Portfolio`,
+        html: `<h3>Name: ${name}</3><br><h3>Email: ${email}</3><br><h3>Subject:  ${subject}</3><br><h3> Message:</3> <br><br> <p>${description}</p>`
+      }
+      // Options for sending mail to the user
+      const UserOptions = {         //For USer
+        from: "karanpal03040@gmail.com",
+        to: email,
+        subject: 'Thank you for sending message!',
+        html: `
+      <p>
       Dear ${name}, <br><br>
       Thank you for reaching out to me. I have received your message and would like to acknowledge your inquiry. 
       I appreciate your interest in my services and am committed to providing you with a prompt and satisfactory response.<br>
@@ -72,40 +81,40 @@ router.post('/contact', async (req, res) => {
       Karan Pal<br>
       Full Stack Developer<br>
       karanpal03040@gmail.com<br>
-      91+886912507<br>
-      </h3>
+      +91886912507<br>
+      </p>
       `
+      }
+      await new Promise((resolve, reject) => {
+        transporterForUser.sendMail(UserOptions, (error, info) => {   // For User
+          if (error) {
+            console.log('Error', error.message)
+            reject(err);
+          } else {
+            console.log('Email Sent Successfully To The User', info.response)
+            resolve(info);
+          }
+        })
+      })
+
+      await new Promise((resolve, reject) => {
+        transporterToMe.sendMail(MyOptions, (error, info) => {     // For Us 
+          if (error) {
+            console.log('Error', error.message)
+            reject(err);
+          } else {
+            console.log('Email Sent Successfully To Me', info.response)
+            resolve(info);
+          }
+        })
+      })
+
+
+
+      return res.status(200).json({ status: 'success', message: 'Thank You ❤' })
+    } catch (error) {
+      res.status(422).json({ error: error.message, message: 'Getting error while calling contact api' })
     }
-    await new Promise((resolve, reject) => {
-      transporterForUser.sendMail(UserOptions, (error, info) => {   // For User
-        if (error) {
-          console.log('Error', error.message)
-          reject(err);
-        } else {
-          console.log('Email Sent Successfully To The User', info.response)
-          resolve(info);
-        }
-      })
-    })
-
-    await new Promise((resolve, reject) => {
-      transporterToMe.sendMail(MyOptions, (error, info) => {     // For Us 
-        if (error) {
-          console.log('Error', error.message)
-          reject(err);
-        } else {
-          console.log('Email Sent Successfully To Me', info.response)
-          resolve(info);
-        }
-      })
-    })
-
-
-
-    return res.status(200).json({ status: 'success', message: 'Data received' })
-  } catch (error) {
-    res.status(404).json({ error: error.message, message: 'Getting error while calling contact api' })
-  }
-})
+  })
 
 module.exports = router;
